f8048231094d69ffa991e6b208cbee225f87e9b7
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-02-01 20:36:17 -0300

    Teste de cores de botão

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("Detector de videos e fotos feitos por IA")
 st.write("Envie uma foto ou vídeo para verificar se existem inconsistências visuais.")
 
 st.sidebar.header("Configurações de Análise")
-conf_threshold = st.sidebar.slider("Confiança Mínima", 0.0, 1.0, 0.5)
+conf_threshold = st.sidebar.slider("Confiança Mínima", 0.0, 1.0, 0.5, color="blue")
 
 # opcao = st.radio("Selecione o que deseja analisar:", ("Foto", "Vídeo", "Monitorar Tela"))
 opcao = st.radio("Selecione o que deseja analisar:", ("Foto", "Vídeo"))
