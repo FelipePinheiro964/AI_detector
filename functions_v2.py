dae107f245c739cb7423fa72d670346f50bdac1d
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-03-29 14:22:04 -0300

    Função score_texture finalizada

--- functions_v2.py
+++ functions_v2.py
@@ -7,3 +7,12 @@ import numpy as np
 def score_texture_uniformity(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     variance = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+    if variance < 50:
+        return 0.75      # muito liso → suspeito de IA
+    elif variance < 150:
+        return 0.40
+    elif variance > 3000:
+        return 0.15      # muito ruidoso → provavelmente real
+    else:
+        return 0.25
